02d1b1c0533ea803f1dd763eeea90a0895eb8e9c
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2025-04-25 11:30:51 -0500

    Start of 3.1.0 edits
    Need to check if the Transform characters switch stocks code interferes with anything again in 50CC.asm

--- Project+ - Brawl Themed/BrawlTP+/Source/Project+/StageTable.asm
+++
@@ -1,6 +0,0 @@
-.include Source/Stagelists/Brawl.asm
-.include Source/Stagelists/PMBR.asm
-.include Source/Stagelists/2023.asm
-.include Source/Stagelists/2024.asm
-.include Source/Stagelists/Aus.asm
-.include Source/Stagelists/Japan.asm
